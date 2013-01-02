b4ff4e203e4f64e092954e904b99bb6f7758787f
Ivo van der Wijk <github@in.m3r.nl>
2013-01-02 18:13:11 +0100

    add wheel specific css, js

---
+++ static/js/wheel.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // make rows clickable if they have a data-url
+    $("tr.clickrow").live('click', function() {
+        document.location = $(this).data('url');
+    });
+});
