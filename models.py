f5ba3eb77ccfe2f92ae303ecdec8d5484d815102
Ivo van der Wijk <github@in.m3r.nl>
2013-01-05 17:39:06 +0100

    issue #471 - update model to hold current template

--- models.py
+++ models.py
@@ -201,6 +201,10 @@ class ContentBase(models.Model):
 
     ## workflow determines possible states and their meaning
     state = models.CharField(max_length=30, blank=True)
+
+    ## identifier of template being used (not path)
+    template = models.CharField(max_length=30, blank=True)
+
     ## one could argue that this can be a property on a node
     navigation = models.BooleanField(default=False)
 
