5a36f72a470a0970f930243714b753eef53d0c0d
Ivo van der Wijk <github@in.m3r.nl>
2013-01-10 15:06:51 +0100

    some comments to satisfy pylint

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,8 +1,15 @@
+"""
+    Test the workflows
+"""
 from wheelcms_axle.workflows.default import DefaultWorkflow
 from wheelcms_axle.tests.models import Type1, Type1Type
 
 class TestDefaultWorkflow(object):
+    """
+        Test the default workflow
+    """
     def test_is_published_true(self):
+        """ verify a spoke is published """
         class DummyContent(object):
             state = "published"
         class DummyType(object):
@@ -11,6 +18,7 @@ class TestDefaultWorkflow(object):
         assert DefaultWorkflow(DummyType()).is_published()
 
     def test_is_published_false(self):
+        """ verify a spoke isn't published """
         class DummyContent(object):
             state = "private"
         class DummyType(object):
@@ -18,21 +26,25 @@ class TestDefaultWorkflow(object):
         assert not DefaultWorkflow(DummyType()).is_published()
 
     def test_content_default(self, client):
+        """ default should be set if not specified """
         data = Type1()
         data.save()
         assert data.state == data.spoke().workflow().default
 
     def test_content_default_update(self, client):
+        """ but left untouched on update """
         data = Type1(state="published")
         data.save()
         assert data.state == "published"
 
     def test_form_choices(self):
+        """ the form should get its choices from the workflow """
         form = Type1Type.form()
         assert form.workflow_choices() == Type1Type.workflowclass.states
         assert form.workflow_default() == Type1Type.workflowclass.default
 
     def test_form_choices_instance(self, client):
+        """ and it's value should be taken from the instance """
         data = Type1(state="published")
         data.save()
         form = Type1Type.form(instance=data)
