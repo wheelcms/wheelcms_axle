dbb763df0b5c88aece9e9386ab8e2b14b5e2957c
Ivo van der Wijk <github@in.m3r.nl>
2013-01-19 18:18:14 +0100

    provide importable default settings - #522

---
+++ settings/__init__.py
@@ -0,0 +1,1 @@
+from wheelcms_axle.settings.settings_userena import *
---
+++ settings/settings_userena.py
@@ -0,0 +1,23 @@
+AUTHENTICATION_BACKENDS = (
+    'userena.backends.UserenaAuthenticationBackend',
+    'guardian.backends.ObjectPermissionBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+AUTH_PROFILE_MODULE="wheelcms_axle.WheelProfile"
+
+ANONYMOUS_USER_ID = -1
+
+LOGIN_REDIRECT_URL = '/accounts/%(username)s/'
+LOGIN_URL = '/accounts/signin/'
+LOGOUT_URL = '/accounts/signout/'
+
+USERENA_SIGNIN_REDIRECT_URL = "/"
+USERENA_ACTIVATION_REQUIRED = True
+
+USERENA_FORBIDDEN_USERNAMES = ('signup', 'signout', 'signin', 'activate', 'me', 'password', 'www', 'wheel', 'root', 'support')
+
+## guardian config
+
+GUARDIAN_RAISE_403 = True
+
