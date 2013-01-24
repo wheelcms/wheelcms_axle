6fdc37d7989aa5229059165e4301899f72278c28
Ivo van der Wijk <github@in.m3r.nl>
2013-01-24 16:08:23 +0100

    let's not forget setup.py itself

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='wheelcms_axle',
+      version=version,
+      description="WheelCMS core package",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Ivo van der Wijk',
+      author_email='wheelcms@in.m3r.nl',
+      url='http://github.com/wheelcms/wheelcms_axle',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'pytest',
+      ],
+      entry_points={
+      },
+
+      )
+
