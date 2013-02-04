ad76af4f91307992a4c0a3f6831743de5c3a0303
Ivo van der Wijk <github@in.m3r.nl>
2013-02-04 16:38:30 +0100

    very simple image insert support

--- wheelcms_axle/settings/settings_tinymce.py
+++ wheelcms_axle/settings/settings_tinymce.py
@@ -12,7 +12,7 @@ TINYMCE_DEFAULT_CONFIG = {
     'table_col_limit' : 5,
     'width':800,
     'height':600,
-    'theme_advanced_buttons1' : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect|wheel_link",
+    'theme_advanced_buttons1' : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect",
     'theme_advanced_buttons2' : "bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,hr,removeformat,visualaid,|,sub,sup,|,charmap",
     'theme_advanced_buttons3': "tablecontrols,|,pastetext,pasteword,selectall",
     'paste_auto_cleanup_on_paste' : True
--- wheelcms_axle/static/tiny_mce/plugins/wheel_browser/editor_plugin.js
+++ wheelcms_axle/static/tiny_mce/plugins/wheel_browser/editor_plugin.js
@@ -6,20 +6,48 @@
                 cmd: 'wheelLink',
                 image: ''
             });
+            ed.addButton('image', {
+                title: 'Add/edit image',
+                cmd: 'wheelImage',
+                image: ''
+            });
             ed.addCommand('wheelLink', function() {
                 var anchor = ed.dom.getParent(ed.selection.getNode(), 'A');
                 var href = "";
                 var title = "";
 
                 if(anchor) {
-			        href = ed.dom.getAttrib(anchor, 'href');
-			        title = ed.dom.getAttrib(anchor, 'title');
+                    href = ed.dom.getAttrib(anchor, 'href');
+                    title = ed.dom.getAttrib(anchor, 'title');
                     console.log("Selection : " + href + ", " + title);
                 }
 
                 ed.getWin().parent.wheel_browser(href, "link",
                                                  function(link) {
                     ed.formatter.apply('link', {'href':link}, anchor);
+                });
+            });
+            ed.addCommand('wheelImage', function() {
+                var anchor = ed.dom.getParent(ed.selection.getNode(), 'A');
+                var href = "";
+                var title = "";
+
+                if(anchor) {
+                    href = ed.dom.getAttrib(anchor, 'href');
+                    title = ed.dom.getAttrib(anchor, 'title');
+                    console.log("Selection : " + href + ", " + title);
+                }
+
+                ed.getWin().parent.wheel_browser(href, "image",
+                                                 function(link) {
+                    // Fixes crash in Safari
+                    if (tinymce.isWebKit) {
+                        ed.getWin().focus();
+                    }
+                    var args = {src:link + '/download'};
+                    ed.execCommand('mceInsertContent', false, ed.dom.createHTML('img', args), {skip_undo : 1});
+                    ed.undoManager.add();
+
                 });
             });
         },
