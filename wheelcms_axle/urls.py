5cec9fce42cefadfde7079b302cd04a4d5141e57
Ivo van der Wijk <github@in.m3r.nl>
2013-02-19 16:06:41 +0100

    path *can* contain / !

--- wheelcms_axle/urls.py
+++ wheelcms_axle/urls.py
@@ -11,10 +11,10 @@ urlpatterns = patterns('',
     twpatterns("/", MainHandler, name="wheel_main", instance=""),
     twpatterns("/\+(?P<action>.+)", MainHandler, name="wheel_main", instance=""),
     ## handle /path/@/create for creation somewhere deeped
-    twpatterns("(?P<parent>[^/]+)/@", MainHandler, name="wheel_main"),
+    twpatterns("(?P<parent>.+)/@", MainHandler, name="wheel_main"),
     ## don't really need this? /<instance>/op works fine...
     twpatterns("(?P<parent>.*)/@/(?P<instance>[^/]*)", MainHandler, name="wheel_main"),
     ## for basic node access:
-    twpatterns("(?P<instance>[^/]+)", MainHandler, name="wheel_main"),
-    twpatterns(r"(?P<instance>[^/]*)/\+(?P<action>.*)", MainHandler, name="wheel_main"),
+    twpatterns("(?P<instance>.+)", MainHandler, name="wheel_main"),
+    twpatterns(r"(?P<instance>.*)/\+(?P<action>.*)", MainHandler, name="wheel_main"),
 )
