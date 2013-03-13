55afe314719f9d0b717b4b5d6d3d0945c2ccdddd
Ivo van der Wijk <github@in.m3r.nl>
2013-03-13 14:19:42 +0100

    do not redirect to logout after login; fixes #576

--- wheelcms_axle/context_processors.py
+++ wheelcms_axle/context_processors.py
@@ -1,7 +1,9 @@
 from wheelcms_axle.models import Configuration
+from django.conf import settings
 
 def configuration(request):
     """ make sure the 'config' context variable is always
         present since it contains information about the current
         theme """
-    return dict(config=Configuration.config())
+    return dict(config=Configuration.config(),
+                settings=settings)
