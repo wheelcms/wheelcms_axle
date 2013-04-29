872d441bacf4781e2b2fc869afa0ea096526627d
Ivo van der Wijk <github@in.m3r.nl>
2013-04-29 12:50:00 +0200

    south support

---
+++ wheelcms_axle/migrations/0001_initial.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Node'
+        db.create_table('wheelcms_axle_node', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('path', self.gf('django.db.models.fields.CharField')(unique=True, max_length=1024)),
+            ('position', self.gf('django.db.models.fields.IntegerField')(default=0)),
+        ))
+        db.send_create_signal('wheelcms_axle', ['Node'])
+
+        # Adding model 'ContentClass'
+        db.create_table('wheelcms_axle_contentclass', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=256)),
+        ))
+        db.send_create_signal('wheelcms_axle', ['ContentClass'])
+
+        # Adding model 'Content'
+        db.create_table('wheelcms_axle_content', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('node', self.gf('django.db.models.fields.related.OneToOneField')(related_name='contentbase', unique=True, null=True, to=orm['wheelcms_axle.Node'])),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('description', self.gf('django.db.models.fields.TextField')(default='', blank=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('publication', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, null=True, blank=True)),
+            ('expire', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2033, 5, 2, 0, 0), null=True, blank=True)),
+            ('state', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('template', self.gf('django.db.models.fields.CharField')(default='', max_length=255, blank=True)),
+            ('navigation', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('meta_type', self.gf('django.db.models.fields.CharField')(max_length=20)),
+            ('owner', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True)),
+        ))
+        db.send_create_signal('wheelcms_axle', ['Content'])
+
+        # Adding M2M table for field classes on 'Content'
+        db.create_table('wheelcms_axle_content_classes', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('content', models.ForeignKey(orm['wheelcms_axle.content'], null=False)),
+            ('contentclass', models.ForeignKey(orm['wheelcms_axle.contentclass'], null=False))
+        ))
+        db.create_unique('wheelcms_axle_content_classes', ['content_id', 'contentclass_id'])
+
+        # Adding model 'WheelProfile'
+        db.create_table('wheelcms_axle_wheelprofile', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('mugshot', self.gf('django.db.models.fields.files.ImageField')(max_length=100, blank=True)),
+            ('privacy', self.gf('django.db.models.fields.CharField')(default='registered', max_length=15)),
+            ('language', self.gf('django.db.models.fields.CharField')(default='en', max_length=5)),
+            ('inform', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('user', self.gf('django.db.models.fields.related.OneToOneField')(related_name='my_profile', unique=True, to=orm['auth.User'])),
+        ))
+        db.send_create_signal('wheelcms_axle', ['WheelProfile'])
+
+        # Adding model 'Configuration'
+        db.create_table('wheelcms_axle_configuration', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(default='', max_length=256, blank=True)),
+            ('description', self.gf('django.db.models.fields.TextField')(default='', blank=True)),
+            ('theme', self.gf('django.db.models.fields.CharField')(default='default', max_length=256, blank=True)),
+            ('analytics', self.gf('django.db.models.fields.CharField')(default='', max_length=50, blank=True)),
+            ('head', self.gf('django.db.models.fields.TextField')(default='', blank=True)),
+        ))
+        db.send_create_signal('wheelcms_axle', ['Configuration'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Node'
+        db.delete_table('wheelcms_axle_node')
+
+        # Deleting model 'ContentClass'
+        db.delete_table('wheelcms_axle_contentclass')
+
+        # Deleting model 'Content'
+        db.delete_table('wheelcms_axle_content')
+
+        # Removing M2M table for field classes on 'Content'
+        db.delete_table('wheelcms_axle_content_classes')
+
+        # Deleting model 'WheelProfile'
+        db.delete_table('wheelcms_axle_wheelprofile')
+
+        # Deleting model 'Configuration'
+        db.delete_table('wheelcms_axle_configuration')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'taggit.tag': {
+            'Meta': {'object_name': 'Tag'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        'taggit.taggeditem': {
+            'Meta': {'object_name': 'TaggedItem'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'taggit_taggeditem_tagged_items'", 'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'taggit_taggeditem_items'", 'to': "orm['taggit.Tag']"})
+        },
+        'wheelcms_axle.configuration': {
+            'Meta': {'object_name': 'Configuration'},
+            'analytics': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '50', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'head': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'theme': ('django.db.models.fields.CharField', [], {'default': "'default'", 'max_length': '256', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '256', 'blank': 'True'})
+        },
+        'wheelcms_axle.content': {
+            'Meta': {'object_name': 'Content'},
+            'classes': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'content'", 'symmetrical': 'False', 'to': "orm['wheelcms_axle.ContentClass']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'expire': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2033, 5, 2, 0, 0)', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'meta_type': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'navigation': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'node': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'contentbase'", 'unique': 'True', 'null': 'True', 'to': "orm['wheelcms_axle.Node']"}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True'}),
+            'publication': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'blank': 'True'}),
+            'state': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'template': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        'wheelcms_axle.contentclass': {
+            'Meta': {'object_name': 'ContentClass'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        'wheelcms_axle.node': {
+            'Meta': {'object_name': 'Node'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'path': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '1024'}),
+            'position': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        'wheelcms_axle.wheelprofile': {
+            'Meta': {'object_name': 'WheelProfile'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'inform': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'language': ('django.db.models.fields.CharField', [], {'default': "'en'", 'max_length': '5'}),
+            'mugshot': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
+            'privacy': ('django.db.models.fields.CharField', [], {'default': "'registered'", 'max_length': '15'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'my_profile'", 'unique': 'True', 'to': "orm['auth.User']"})
+        }
+    }
+
+    complete_apps = ['wheelcms_axle']
