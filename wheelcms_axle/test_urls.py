9393bba0c9bdad12ebaae781d4cd2ba6a60e3198
Ivo van der Wijk <github@in.m3r.nl>
2013-06-30 13:39:54 +0200

    disable some patterns

--- wheelcms_axle/test_urls.py
+++ wheelcms_axle/test_urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import patterns, include
 
 urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-    (r'^favicon.ico$', 'django.views.generic.simple.redirect_to',
-                        {'url': '/static/images/favicon.ico'}),
+#    (r'^admin/', include(admin.site.urls)),
+#    (r'^favicon.ico$', 'django.views.generic.simple.redirect_to',
+#                        {'url': '/static/images/favicon.ico'}),
     (r'^tinymce/', include('tinymce.urls')),
     (r'^accounts/', include('userena.urls')),
     (r'^search/', include('haystack.urls')),
