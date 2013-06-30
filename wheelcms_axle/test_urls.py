9b65315c422310f8b0585e67e00479a9c2736f34
Ivo van der Wijk <github@in.m3r.nl>
2013-06-30 13:47:33 +0200

    further pattern reduction

--- wheelcms_axle/test_urls.py
+++ wheelcms_axle/test_urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls.defaults import patterns, include
 
 urlpatterns = patterns('',
-#    (r'^admin/', include(admin.site.urls)),
-#    (r'^favicon.ico$', 'django.views.generic.simple.redirect_to',
-#                        {'url': '/static/images/favicon.ico'}),
-    (r'^tinymce/', include('tinymce.urls')),
     (r'^accounts/', include('userena.urls')),
     (r'^search/', include('haystack.urls')),
     (r'', include('wheelcms_axle.urls')),
