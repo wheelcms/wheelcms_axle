ee2e339e32b9478386e1d073fb87b1b74daaee96
Ivo van der Wijk <github@in.m3r.nl>
2013-06-30 14:18:40 +0200

    travis ci testing setup

---
+++ quicktest.py
@@ -0,0 +1,31 @@
+from twotest.quicktest import QuickDjangoTest
+
+if __name__ == '__main__':
+    QuickDjangoTest(
+        (
+            'django.contrib.sites',
+            'django.contrib.messages',
+            'django.contrib.staticfiles',
+            'django.contrib.humanize',
+
+            'south',
+            'haystack',
+
+            'wheelcms_axle',
+
+            'taggit',
+            'two.ol',
+            'two.bootstrap',
+            'twotest',
+            'wheelcms_axle.tests',
+
+        ),
+        ROOT_URLCONF="wheelcms_axle.test_urls",
+        ANONYMOUS_USER_ID=-1,
+        HAYSTACK_SITECONF = 'wheelcms_axle.search_sites',
+        HAYSTACK_SEARCH_ENGINE = 'simple',
+        AUTH_PROFILE_MODULE="wheelcms_axle.WheelProfile",
+        CLEANUP_MEDIA=True,
+        TEST_MEDIA_ROOT="/tmp/wheelcms_axle_test",
+        USE_TZ=True
+    )
