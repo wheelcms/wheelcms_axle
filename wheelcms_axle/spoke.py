57ad9ecc12974680986d5f572fdb4f2abf483e75
Ivo van der Wijk <github@in.m3r.nl>
2013-07-15 15:36:18 +0200

    index get_absolute_url now in stead of 'path'

--- wheelcms_axle/spoke.py
+++ wheelcms_axle/spoke.py
@@ -50,7 +50,7 @@ def indexfactory(spoke):
         meta_type = indexes.CharField(stored=True, indexed=True,
                                       model_attr='meta_type')
         path = SpokeCharField(spoke=spoke, stored=True, indexed=True,
-                              model_attr='path')
+                              model_attr='get_absolute_url')
         slug = indexes.CharField(stored=True, indexed=True,
                                   model_attr='node__slug')
         created = indexes.DateField(stored=True, indexed=True,
