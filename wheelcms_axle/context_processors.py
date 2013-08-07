b230b82a489c4b4ae19733a94f766857cfb50dac
Ivo van der Wijk <github@in.m3r.nl>
2013-08-07 14:40:07 +0200

    issue 576 fixed for different url bases

--- wheelcms_axle/context_processors.py
+++ wheelcms_axle/context_processors.py
@@ -2,15 +2,21 @@ from django.conf import settings
 from wheelcms_axle.models import Configuration
 from wheelcms_axle.node import Node
 from wheelcms_axle.toolbar import Toolbar
+from django.core.urlresolvers import reverse
+
 
 
 def configuration(request):
     """ make sure the 'config' context variable is always
         present since it contains information about the current
         theme """
+    def is_logout_url():
+        return request.path == reverse("userena_signout")
+
     return dict(config=Configuration.config(),
                 settings=settings,
-                root=Node.root())
+                root=Node.root(),
+                is_logout_url=is_logout_url)
 
 def toolbar(request):
     if request.user.is_authenticated():
