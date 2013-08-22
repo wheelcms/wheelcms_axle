8f51027492217567e2a24583a4045ade2d9a5502
Ivo van der Wijk <github@in.m3r.nl>
2013-08-22 14:20:49 +0200

    allow explicit setting of language

--- wheelcms_axle/utils.py
+++ wheelcms_axle/utils.py
@@ -1,4 +1,5 @@
 from django.utils import translation
+from django.conf import settings
 
 def get_url_for_language(o, language):
     """ assumes 'o' has a get_absolute_url """
@@ -9,6 +10,9 @@ def get_url_for_language(o, language):
     return url
 
 def get_active_language(request):
+    if settings.FORCE_LANGUAGE:
+        return settings.FORCE_LANGUAGE
+
     if request:
         admin_language = request.session.get('admin_language')
         return admin_language or request.GET.get('language', translation.get_language())
