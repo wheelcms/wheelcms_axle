186653330b095053b24cee92957944edc35245e6
Ivo van der Wijk <github@in.m3r.nl>
2013-08-22 16:52:08 +0200

    i18n support for topnav

--- wheelcms_axle/queries.py
+++ wheelcms_axle/queries.py
@@ -1,16 +1,20 @@
 from wheelcms_axle.models import Node
 
-def get_visible_children(node):
+def get_visible_children(node, language=None):
     """
         Return all childen of a node that
         are published and in navigation,
         ordered by position
     """
-    return node.childrenq(contentbase__state="published",
-                          contentbase__navigation=True)
+    q = dict(contentbase__state="published",
+             contentbase__navigation=True)
+    if language:
+        q['contentbase__language'] = language
 
-def toplevel_visible_children():
+    return node.childrenq(**q)
+
+def toplevel_visible_children(language=None):
     """
         Return all toplevel visible nodes
     """
-    return get_visible_children(Node.root())
+    return get_visible_children(Node.root(), language=language)
