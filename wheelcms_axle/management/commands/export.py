ea3dcba1e836c35475e71322ce08b8d5e4763f34
Ivo van der Wijk <github@in.m3r.nl>
2013-09-12 17:24:41 +0200

    Make command more mature; proper handle/parse arguments

--- wheelcms_axle/management/commands/export.py
+++ wheelcms_axle/management/commands/export.py
@@ -1,10 +1,11 @@
 import os
 import shutil
+from optparse import make_option
 
 from xml.etree import ElementTree
 from xml.dom import minidom
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
 from wheelcms_axle.models import Node
@@ -21,10 +22,22 @@ def prettify(elem):
 
 class Command(BaseCommand):
     """ Normalize existing usernames to match allowed characters """
-    args = 'writeto [path]'
+    args = '[folder-to-export-to]'
     help = 'Export (part of) a site as XML'
 
-    def handle(self, writeto, path="", *args, **options):
+    base_options = (
+        make_option("-q", "--quiet", action="store_false", dest="verbose",
+                    default=True, help="Be quiet"),
+    )
+
+    option_list = BaseCommand.option_list + base_options
+
+    def handle(self, writeto=None, path="", *args, **options):
+        if writeto is None:
+            raise CommandError("You must specificy a writable directory where the export can be written to")
+
+        verbose = options.get('verbose', True)
+
         mediadir = os.path.join(writeto, "media")
         if not os.path.exists(mediadir):
             os.makedirs(mediadir)
@@ -32,8 +45,8 @@ class Command(BaseCommand):
         base = path # "" # /sub5_2/sub4_2/sub3_2"
         root = Node.get(base)
         xml, files = Exporter().run(root, base)
-        open(os.path.join(writeto, "content.xml"),
-             "w").write(prettify(xml).encode("utf8"))
+        open(os.path.join(writeto, "content.xml"), "w"
+            ).write(prettify(xml).encode("utf8"))
         for file in set(files):
             dirname = os.path.dirname(file)
             if dirname:
@@ -43,7 +56,9 @@ class Command(BaseCommand):
             dest = os.path.join(mediadir, file)
             source = os.path.join(settings.MEDIA_ROOT, file)
 
-            print "Copy %s to %s" % (source, dest)
+            if verbose:
+                print "Copy %s to %s" % (source, dest)
             shutil.copy(source, dest)
 
-        print files
+        if verbose:
+            print files
