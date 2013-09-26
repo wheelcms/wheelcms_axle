0fcfb10d67ed05cafc01870a6fda4e0602e9ae84
Ivo van der Wijk <github@in.m3r.nl>
2013-09-26 17:41:19 +0200

    rename test_urls so it won't be collected by pytest

--- wheelcms_axle/test_urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import patterns, include
-
-urlpatterns = patterns('',
-    (r'^accounts/', include('userena.urls')),
-    (r'^search/', include('haystack.urls')),
-    (r'', include('wheelcms_axle.urls')),
-)
-
