a4d41300bb54a121ea98662e1ee033560ce14cc3
Ivo van der Wijk <github@in.m3r.nl>
2014-04-11 15:51:47 +0200

    finalize django 1.6.x support

--- wheelcms_axle/registries/fragments.py
+++
@@ -1,9 +0,0 @@
-from wheelcms_axle.registry import Registry
-
-class FragmentsRegistry(dict):
-    def register(self, name, template, priority=100):
-        if name not in self:
-            self[name] = []
-        self[name].append((priority, template))
-
-fragments_registry = Registry(FragmentsRegistry())
