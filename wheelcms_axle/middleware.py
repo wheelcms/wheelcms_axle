8627a1a315b6dabc0f67e75e01ecd9299c891742
Ivo van der Wijk <github@in.m3r.nl>
2014-04-12 19:18:03 +0200

    Workaround for django 1.6 cookie issue

---
+++ wheelcms_axle/middleware.py
@@ -0,0 +1,31 @@
+from django.contrib.messages.storage.cookie import CookieStorage
+
+class FixMessageMiddleware(object):
+    """
+        The message cookie format has changed from Django 1.4 to
+        Django 1.5. Django 1.5 probably supported both formats,
+        but if you move from 1.4.x to 1.6.x directly, you will run into
+        an IndexError:
+
+        Exception Type: IndexError
+        Exception Value:
+        list index out of range
+        Exception Location: (...) django/contrib/messages/storage/cookie.py in process_messages, line 37
+
+        This small piece of middleware will track those cookies and destroy
+        them (leaving new-style in tact)
+
+        https://code.djangoproject.com/ticket/22426
+
+        DISCLAIMER:
+
+        This middleware will not attempt to rewrite the messages! You may
+        miss important notifications because of this!
+    """
+    def process_request(self, request):
+        data = request.COOKIES.get("messages")
+        storage = CookieStorage(request)
+        try:
+            storage._decode(data)
+        except IndexError:
+            del request.COOKIES['messages']
